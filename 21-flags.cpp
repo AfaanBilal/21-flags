b24eeb29f82a6a160095cca5e111d0f68b2a1cde
Afaan Bilal <afaanbilal@gmail.com>
2017-11-09 22:40:15 +0530

    Initial commit

---
+++ 21-flags.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+
+using namespace std;
+
+const int PLAYER_COMPUTER = 1;
+const int PLAYER_PERSON   = 2;
+
+int flags = 21;
+int currentPlayer = PLAYER_COMPUTER;
+bool hasWon = false;
+
+void displayFlags();
+void checkIfWon();
+
+int main()
+{
+    cout << "\t\t\t21 Flags" << endl;
+    cout << "\thttps://github.com/AfaanBilal/21-flags" << endl;
+    cout << endl << endl;
+    cout << "\tRules:" << endl;
+    cout << "\t1. There are 21 flags." << endl;
+    cout << "\t2. Each player can take 1, 2 or 3 flags in each turn." << endl;
+    cout << "\t3. The player to take the last flag wins." << endl;
+    cout << endl;
+    cout << "\tYou are playing against the computer." << endl;
+    cout << "\tDo you want to go first? (y/n) : ";
+    char choice = 'n';
+    cin >> choice;
+
+    if (choice == 'y' || choice == 'Y')
+        currentPlayer = PLAYER_PERSON;
+    
+    while (flags > 0 && !hasWon)
+    {
+        displayFlags();
+        switch (currentPlayer)
+        {
+            case PLAYER_PERSON:
+                {
+                    int flagsToTake = 0;
+                    cout << endl << "\tNumber of flags you want to take: ";
+                    cin >> flagsToTake;
+                    if (flagsToTake > 3 || flagsToTake < 1)
+                    {
+                        cout << "\tYou can only take 1, 2 or 3 flags at each turn." << endl;
+                        break;
+                    }
+
+                    flags -= flagsToTake;
+                    cout << "\tYou took " << flagsToTake << " flag(s)." << endl;
+                    currentPlayer = PLAYER_COMPUTER;
+                }
+                break;
+            
+            case PLAYER_COMPUTER:
+                {
+                    int flagsToTake = flags % 4;
+                    if (flagsToTake == 0)
+                        flagsToTake = 1;
+                    flags -= flagsToTake;
+                    cout << "\tComputer took " << flagsToTake << " flag(s)." << endl;
+                    currentPlayer = PLAYER_PERSON;
+                }
+                break;
+        }
+
+        checkIfWon();
+    }
+    
+    cin.clear();
+    cin.ignore(100, '\n');
+}
+
+void displayFlags()
+{
+    cout << endl;
+    cout << "\t";
+    for (int i = 1; i <= 21; i++)
+    {
+        if (i > flags)
+            cout << "* | ";
+        else
+            cout << i << " | ";
+    }
+    cout << endl;
+}
+
+void checkIfWon()
+{
+    if (flags == 0)
+    {
+        hasWon = true;
+        displayFlags();
+        if (currentPlayer == PLAYER_COMPUTER)
+        {
+            cout << endl << "\t\t\t\tCongratulations! You have won!" << endl;
+        }
+        else
+        {
+            cout << endl << "\t\t\t\tSorry! The computer has won!" << endl;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+21 Flags
+=======
+
+Author: **Afaan Bilal ([@AfaanBilal](https://github.com/AfaanBilal))**   
+Author URL: **[Google+](https://google.com/+AfaanBilal)**
+
+## Introduction
+**21 Flags** is a simple program in C++ to play the Game Theory based game *21 flags* in a console window.  
+Inspriration: My dear friend [Anees Muzaffar's 21 Flags](https://github.com/AneesMuzaffar/21-Flags).
+
+## Contributing
+All contributions are welcome. Please create an issue first for any feature request
+or bug. Then fork the repository, create a branch and make any changes to fix the bug 
+or add the feature and create a pull request. That's it!
+Thanks!
+
+## License
+**21 Flags** is released under the MIT License.
+Check out the full license [here](LICENSE).
